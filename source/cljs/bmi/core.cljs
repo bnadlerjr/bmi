fa1b1606f2c62536b468ea7ca52844a718d6512e
Bob Nadler <bnadlerjr@gmail.com>
2018-07-22 16:11:14 -0400

    Height and weight controls for BMI component

--- source/cljs/bmi/core.cljs
+++ source/cljs/bmi/core.cljs
@@ -1,7 +1,35 @@
-(ns bmi.core)
+(ns bmi.core
+  (:require [reagent.core :as reagent]))
+
+(defn bmi-component
+  []
+  (let [state (reagent/atom {:height 180 :weight 80})]
+    (fn []
+      [:div
+       [:h1 "BMI Calculator"]
+       [:div "Height: " (:height @state) "cm"
+        [:input {:type "range"
+                 :value (:height @state)
+                 :min 100
+                 :max 220
+                 :on-change (fn [e]
+                              (swap! state assoc :height (.-target.value e)))}]]
+       [:div "Weight: " (:weight @state) "kg"
+        [:input {:type "range"
+                 :value (:weight @state)
+                 :min 30
+                 :max 150
+                 :on-change (fn [e]
+                              (swap! state assoc :weight (.-target.value e)))}]]])))
 
 (defn start
-  [])
+  []
+  (reagent/render [bmi-component]
+                  (.getElementById js/document "bmi-component")))
 
 (defn stop
   [])
+
+(defn ^:export init
+  []
+  (start))
